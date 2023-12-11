f1ef85a5e752a5d36d4e584770e3db1d312b9c8a
MitchellBond <Mitchell.Bond@skybettingandgaming.com>
2023-12-11 20:28:38 +0000

    day 8

--- 2023/day_08/day_08.py
+++ 2023/day_08/day_08.py
@@ -1,6 +1,6 @@
 import argparse
-from helpers import read_input, Node, node_cycle, execute_instructions
-
+from helpers import read_input, Node, find_first_z_node
+from math import lcm
 
 def part_1(instructions: str, node_map: dict) -> int:
     steps = 0
@@ -14,30 +14,11 @@ def part_1(instructions: str, node_map: dict) -> int:
 
 
 def part_2(instructions: str, node_map: dict) -> int:
-    steps = 0
     instructions = 50 * instructions
-    n_instructions = len(instructions)
-    nodes = [node.value for node in node_map.values() if node.value[-1] == "A"]
-    node_summary = {}
-    for node in node_map.values():
-        path = execute_instructions(node, node_map, instructions)
-        node_summary[node.value] = (path[-1], [i + 1 for i, node in enumerate(path) if node[-1] == "Z"])
+    nodes = [node for node in node_map.values() if node.value[-1] == "A"]
+    first_match = [find_first_z_node(node, node_map, instructions) for node in nodes]
+    return lcm(*first_match)
 
-
-    end = False
-    while not end:
-        vals = [node_summary[node] for node in nodes]
-        nodes = [v[0] for v in vals]
-        matches = [v[1] for v in vals]
-        if all([len(m) > 0 for m in matches]):
-            common_vals = set.intersection(*map(set, matches))
-
-            if len(common_vals) > 0:
-                return steps * n_instructions + min(common_vals)
-        if steps % 100000 == 0:
-            print(steps * n_instructions)
-        steps += 1
-    return None
 
 
 if __name__ == "__main__":
@@ -49,10 +30,9 @@ if __name__ == "__main__":
     instructions = puzzle_input[0]
     nodes = [Node(line) for line in puzzle_input[2:]]
     node_map = {node.value: node for node in nodes}
-    #
-    # print("Part 1:")
-    # print(part_1(instructions, node_map))
-
+    print("Part 1:")
+    print(part_1(instructions, node_map))
+    
     print("Part 2:")
     print(part_2(instructions, node_map))
 
--- 2023/day_08/helpers.py
+++ 2023/day_08/helpers.py
@@ -23,23 +23,12 @@ class Node:
         return hash(self.value)
 
 
-def node_cycle(node_map: dict, instructions: str, start_node: Node) -> list[str]:
-    i = 0
+def find_first_z_node(node: Node, node_map: dict, instructions: str) -> int:
+    steps = 0
     n_instructions = len(instructions)
-    nodes = [start_node.value]
-    next_node = node_map[start_node.map(instructions[i % n_instructions])]
-    while next_node != start_node or not next_node.z_steady_state():
-        i += 1
-        nodes += [next_node.value]
-        next_node = node_map[start_node.map(instructions[i % n_instructions])]
-        if next_node.value[-1] == "Z":
-            print(f"{i}: {next_node.value}")
-    return nodes
-
-
-def execute_instructions(node: Node, node_map: dict, instructions: str) -> list[str]:
-    nodes = []
-    for i in instructions:
-        node = node_map[node.map(i)]
-        nodes += [node.value]
-    return nodes
+    index_of_z = []
+    while len(index_of_z) == 0:
+        node = node_map[node.map(instructions[steps % n_instructions])]
+        steps += 1
+        if node.value[-1] == "Z":
+            return steps
