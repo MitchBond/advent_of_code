0366437bf75b1d4f326c654e0cbc02d0f96f4457
Mitch Bond <mitch@mac.home>
2024-12-02 15:24:33 +0000

    not using struct

--- 2024/rust/day_02/src/main.rs
+++ 2024/rust/day_02/src/main.rs
@@ -5,74 +5,61 @@ fn read_input(is_practice: bool) -> String {
         "input/practice_input.txt"
     } else {
         "input/full_input.txt"
-    }.to_string();
-    
+    }
+    .to_string();
+
     let contents = fs::read_to_string(file_name).expect("Should be able to read file");
-    return contents
+    return contents;
 }
 
-
-struct Report {
-    levels: Vec<i32>
-}
-
-impl Report {
-    fn diffs(&self) -> Vec<i32> {
+    fn diffs(levels: &Vec<i32>) -> Vec<i32> {
         let mut diffs = Vec::new();
-        let without_first = &self.levels[1..];
-        let without_last = &self.levels[..(self.levels.len() - 1)];
-        for (x, next_x) in without_last.iter().zip(without_first.iter()){
+        let without_first = &levels[1..];
+        let without_last = &levels[..(levels.len() - 1)];
+        for (x, next_x) in without_last.iter().zip(without_first.iter()) {
             diffs.push(next_x - x);
         }
-        return diffs
+        return diffs;
     }
-    fn is_valid(&self) -> bool {
-        let diffs = self.diffs();
+    fn is_valid(levels: &Vec<i32>) -> bool {
+        let diffs = diffs(&levels);
         let strictly_increasing = diffs.iter().all(|x| x > &0);
         let strictly_decreasing = diffs.iter().all(|x| x < &0);
         let max_diff = diffs.iter().all(|x| x.abs() <= 3);
         return (strictly_increasing || strictly_decreasing) && max_diff;
     }
 
-    fn is_valid_2(&self) -> bool {
-        if self.is_valid() {
+    fn is_valid_2(levels: &Vec<i32>) -> bool {
+        if is_valid(&levels) {
             return true;
         } else {
-            for i in 0..self.levels.len() {
-                let mut sublist = self.levels.clone();
+            for i in 0..levels.len() {
+                let mut sublist = levels.clone();
                 sublist.remove(i);
-                let report = Report {levels : sublist};
-                if report.is_valid(){
+                if is_valid(&sublist) {
                     return true;
                 }
-
             }
             return false;
         }
-
-
-
     }
+
+
+fn parse_input(input: String) -> Vec<Vec<i32>> {
+    return input
+        .split("\n")
+        .map(|line| line.split(" ").map(|num| num.parse::<i32>().unwrap()).collect()).collect();
+
 }
 
-
-fn parse_input(input: String) -> Vec<Report> {
-    let lines = input.split("\n").map(
-        |line| line.split(" ").map(|num| num.parse::<i32>().unwrap())
-    );
-
-    return lines.map(|level| Report { levels: level.collect() }).collect()
+fn part_1(input: &Vec<Vec<i32>>) -> usize {
+    let valid_reports: Vec<&Vec<i32>> = input.iter().filter(|report| is_valid(&report)).collect();
+    return valid_reports.len();
 }
 
-
-fn part_1(input: &Vec<Report>) -> usize { 
-    let valid_reports: Vec<&Report> = input.iter().filter(|report| report.is_valid()).collect();
-    return valid_reports.len()
-}
-
-fn part_2(input: &Vec<Report>) -> usize {
-    let valid_reports: Vec<&Report> = input.iter().filter(|report| report.is_valid_2()).collect();
-    return valid_reports.len()
+fn part_2(input: &Vec<Vec<i32>>) -> usize {
+    let valid_reports: Vec<&Vec<i32>> = input.iter().filter(|report| is_valid_2(&report)).collect();
+    return valid_reports.len();
 }
 
 fn main() {
