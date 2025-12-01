f573586bfac89b5bcccf9f07cf465588e507dce8
Mitch Bond <m.bond@draftkings.com>
2025-12-01 19:21:54 +0000

    rename

--- 2025/rust/day_01/src/main.rs
+++ 2025/rust/day_01/src/main.rs
@@ -33,12 +33,12 @@ fn part_2(instructions: &Vec<Instruction>) -> i32 {
     let mut zero_count: i32 = 0;
 
     for instruction in instructions {
-        let offset = if instruction.direction == -1 && pos > 0{
+        let dist_from_zero = if instruction.direction == -1 && pos > 0{
         100 - pos
     } else {
         pos
     };
-        let num_zero_passed = (offset + instruction.distance).div_euclid(100);
+        let num_zero_passed = (dist_from_zero + instruction.distance).div_euclid(100);
         zero_count += num_zero_passed;
 
         pos += instruction.direction * instruction.distance;
